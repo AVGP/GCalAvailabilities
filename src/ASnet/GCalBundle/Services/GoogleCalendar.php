8234b0c5e326e292264f230c3623bfe55db22e89
Martin N <mr.avgp@googlemail.com>
2012-03-28 19:46:31 +0200

    Setting up a google_calendar service, the class and unit tests for it. Currently as empty stubs.

---
+++ src/ASnet/GCalBundle/Services/GoogleCalendar.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ASnet\GCalBundle\Services;
+
+use \Zend_Gdata_Calendar;
+
+/**
+ * Provides a wrapper for the external Zend_Gdata_Calendar class
+ * This service helps us to easily utilize Zend_Gdata and capsule it, so we can
+ * easily test our code.
+ */
+class GoogleCalendar {
+    
+}
+
+?>
---
+++ src/ASnet/GCalBundle/Tests/Services/GoogleCalendarTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ASnet\GCalBundle\Tests\Service;
+
+/**
+ * Testing the GoogleCalendar service
+ */
+class GoogleCalendarTest extends PHPUnit_Framework_TestCase {
+    
+}
+
+?>
