808264016cbc94e550e600abd73ebd934f4bf37d
Martin N <mr.avgp@googlemail.com>
2012-03-28 20:33:36 +0200

    Whoops, the autoloader. Added the Zend_ prefix to the Autoloader.

--- app/autoload.php
+++ app/autoload.php
@@ -18,6 +18,7 @@ $loader->registerNamespaces(array(
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
     'Twig_'            => __DIR__.'/../vendor/twig/lib',
+    'Zend_'            => __DIR__.'/../vendor/zend/lib',
 ));
 
 // intl
